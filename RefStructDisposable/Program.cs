7e1d374b57be0ed75c043a0b4dd81f32dcfbba63
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-05-04 21:27:06 +0200

    Initial commit

---
+++ RefStructDisposable/Program.cs
@@ -0,0 +1,15 @@
+﻿using var test = DisposableStruct.Create(() => Console.WriteLine("Hello, World!"));
+
+internal readonly ref struct DisposableStruct : IDisposable
+{
+    private readonly Action _dispose;
+
+    private DisposableStruct(Action dispose)
+    {
+        _dispose = dispose;
+    }
+
+    public static DisposableStruct Create(Action dispose) => new(dispose);
+
+    public void Dispose() => _dispose();
+}
